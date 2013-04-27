491ff63e1d8f019ac25606a85750e642417b5cea
kliu <kliu@kliu-virtual-machine.(none)>
2013-04-27 15:30:18 +0800

    update test_template.c

--- test/test_template.c
+++ test/test_template.c
@@ -2,32 +2,48 @@
 #include "model/lattice_net.h"
 #include "package/net_experiment.h"
 #include "operator/net_constructor.h"
+#include "statistic/stat_selector.h"
+#include "statistic/stat_distance.h"
+#include "statistic/stat_on_lattice_net.h"
+
+#include <stdio.h>
 
 struct private_ctx{
+  Selector *center_sel;
+  Selector *edge_sel;
+  Distance *distance_cal;
 };
 
 /**
  * add operations you want to do when the net size is fixed
  */
 static int after_size(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
+  struct private_ctx *ctx = pak->ctx;
+  
+  /**
+   * you should initalize the selctor and distance_cal here
+   */
 }
 
 /**
  * add operations when the net is connected
  */
 static int after_connect(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
+  struct private_ctx *ctx = pak->ctx;
 }
 
 /**
  * add operations when the nodes' capacity has been calculated
  */
 static int after_capacity(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
+  struct private_ctx *ctx = pak->ctx;
 }
 
 /**
  * add operations when the first round of random attack happened
  */
 static int after_attack(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
+  struct private_ctx *ctx = pak->ctx;
 }
 
 /**
@@ -36,12 +52,14 @@ static int after_attack(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
  *
  */
 static int after_loop(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
+  struct private_ctx *ctx = pak->ctx;
 }
 
 /**
  * add operations after the net went steady
  */
 static int after_steady(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
+  struct private_ctx *ctx = pak->ctx;
 }
 
 /**
@@ -50,6 +68,7 @@ static int after_steady(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
  *
  */
 static int after_destroy(Net *net, BC *bc, Exp_State *state, Net_Exp_Pak *pak){
+  struct private_ctx *ctx = pak->ctx;
 }
 
 /**
@@ -80,9 +99,18 @@ int main(int argc, char **argv){
   }
 
   package->ctx = context;
+  package->opers[OPER_AFTER_SIZE]        = after_size;
+  package->opers[OPER_AFTER_CONNECT]     = after_connect;
+  package->opers[OPER_AFTER_CAPACITY]    = after_capacity;
+  package->opers[OPER_AFTER_ATTACK]      = after_attack;
+  package->opers[OPER_AFTER_EACH_LOOP]   = after_loop;
+  package->opers[OPER_AFTER_STEADY]      = after_steady;
+  package->opers[OPER_DESTROY]           = after_destroy;
+
   net_experiment_add_package(package, net_exp);
   net_experiment_run(argv[1], net_exp);
   net_experiment_destroy(net_exp);
+
   return 0;
 }
 
